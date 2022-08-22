05a86e1dcc8af069ff8d43f89e83a2579a3bf6a2
haeinkang <haein.kang7@gmail.com>
2022-08-22 17:36:00 +0900

    route 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path=":id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
