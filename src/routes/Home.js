6006bd5454f480a1f7e644df39371a1f9cf07568
haeinkang <haein.kang7@gmail.com>
2022-08-22 18:19:46 +0900

    3.0 Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home(props) {
+  const [text, setText] = useState('')
+
+  function onChange(e) {
+    setText(e.target.value)
+  }
+
+  function onSubmit(e) {
+    e.preventDefault()
+    console.log(text)
+    setText("")
+  }
+
   return (
-    <div>
-      HOME
-    </div>
+    <React.Fragment>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+    </React.Fragment>
   );
 }
 
